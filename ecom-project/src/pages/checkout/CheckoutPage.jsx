2890a1379e3705d3a5c79a2a33419102797cc8ff
daaaanav <dj631125@gmail.com>
2026-02-06 22:43:35 +0530

    CartUpdated

--- ecom-project/src/pages/checkout/CheckoutPage.jsx
+++ ecom-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,49 +1,50 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import './CheckoutPage.css'
-import './checkout-header.css'
+import './CheckoutPage.css';
+import './checkout-header.css';
 import { OrderSummary } from './OrderSummary';
 import { PaymentSummary } from './PaymentSummary';
 
-export function CheckoutPage({cart}) {
+export function CheckoutPage({ cart, loadCart }) {
     const [deliveryOption, setDeliveryOptions] = useState([]);
-    const [paymentSummary, setPaymentSummary] = useState([null])
+    const [paymentSummary, setPaymentSummary] = useState([null]);
 
-    useEffect(() =>{
+    useEffect(() => {
+        const fetchCheckoutData = async () => {
+            let response = await axios.get(
+                '/api/delivery-options?expand=estimatedDeliveryTime',
+            );
 
-        const fetechCheckoutData = async() =>{
+            setDeliveryOptions(response.data);
 
-        let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
-
-            setDeliveryOptions(response.data)
-        
-
-        response = await axios.get('/api/payment-summary')
-
-            setPaymentSummary(response.data)
-       
-
+            response = await axios.get('/api/payment-summary');
+            setPaymentSummary(response.data);
         };
 
-       fetechCheckoutData();
-
-    },[])
+        fetchCheckoutData();
+    }, [cart]);
 
     return (
         <>
-        <title>Checkout</title>
+            <title>Checkout</title>
             <div className="checkout-header">
                 <div className="header-content">
                     <div className="checkout-header-left-section">
                         <a href="/">
                             <img className="logo" src="images/logo.png" />
-                            <img className="mobile-logo" src="images/mobile-logo.png" />
+                            <img
+                                className="mobile-logo"
+                                src="images/mobile-logo.png"
+                            />
                         </a>
                     </div>
 
                     <div className="checkout-header-middle-section">
-                        Checkout (<a className="return-to-home-link"
-                            href="/">3 items</a>)
+                        Checkout (
+                        <a className="return-to-home-link" href="/">
+                            3 items
+                        </a>
+                        )
                     </div>
 
                     <div className="checkout-header-right-section">
@@ -56,9 +57,13 @@ export function CheckoutPage({cart}) {
                 <div className="page-title">Review your order</div>
 
                 <div className="checkout-grid">
-                <OrderSummary cart={cart} deliveryOption={deliveryOption}/>
+                    <OrderSummary
+                        cart={cart}
+                        deliveryOption={deliveryOption}
+                        loadCart={loadCart}
+                    />
 
-                    <PaymentSummary paymentSummary={paymentSummary}/>
+                    <PaymentSummary paymentSummary={paymentSummary} />
                 </div>
             </div>
         </>
